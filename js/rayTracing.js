3993b074ecf8b37cb30f3e7eeec4dc6808893eaf
arnaud droxler <arnaud.droxler@gmail.com>
2016-12-25 15:16:43 +0100

    corectif des ombres deplacement de la ball grace a un lance de rayon dans la scene

--- js/rayTracing.js
+++ js/rayTracing.js
@@ -8,14 +8,15 @@ var mvMatrix = mat4.create();
 var pMatrix = mat4.create();
 var repMatrix = mat3.create();
 
-var rotY = 0; //rotation on the Y-axis (in degrees)
-var rotX = 0; //rotation on the X-axis (in degrees)
+var rotY = 0;
+var rotX = 0;
 var dragging = false;
+var sphereMoving = false;
 var oldMousePos = {x: 0, y: 0};
-var mousePos;
-var rotSpeed = 1.0; //rotation speed
-var mouseButton;
+var alpha = 0;
+var fov = 70;
 
+var spherePos = vec3.fromValues(0.0,-0.8,0.0);
 
 window.onload = function() {
     canvas = document.getElementById('glcanvas');
@@ -64,6 +65,7 @@ function initProgram() {
     prg.repMatrixUniform        = gl.getUniformLocation(prg, 'uRepMatrix');
     prg.alphaUniform            = gl.getUniformLocation(prg, 'uAlpha');
     prg.timeUniform             = gl.getUniformLocation(prg, 'uTimeSinceStart');
+    prg.spherePosUniform           = gl.getUniformLocation(prg, 'uSpherePos');
 
 }
 
@@ -121,8 +123,7 @@ function drawScene(){
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
 
-    var fov = 70;
-    var alpha = Math.tan(fov/2*Math.PI/180);
+    alpha = Math.tan(fov/2*Math.PI/180);
 
     mat4.identity(pMatrix);
     mat4.identity(mvMatrix);
@@ -135,8 +136,10 @@ function drawScene(){
     repMatrix = mat3.fromValues(2/gl.viewportWidth,0,0,0,-(2/gl.viewportHeight),0,-1,1,1);
     var time = new Date().getTime();
 
+    var pos = vec3.create();
+    vec3.multiply(pos,spherePos,[1.0,-1.0,1.0]);
 
-
+    gl.uniform3fv(prg.spherePosUniform,pos);
     gl.uniform1f(prg.alphaUniform, alpha);
     gl.uniformMatrix4fv(prg.pMatrixUniform, false, pMatrix);
     gl.uniformMatrix4fv(prg.mvMatrixUniform, false, mvMatrix);
@@ -153,34 +156,89 @@ function degToRad(degrees) {
     return (degrees * Math.PI / 180.0);
 }
 
+function intersectSphere( dir, origin, spherePosition, sphereRayon) {
+    var l = vec3.fromValues(0.0,0.0,0.0);
+    vec3.sub(l,origin,spherePosition);
+    var b = 2.0 * vec3.dot(l, dir);
+    var c = vec3.dot(l, l) - sphereRayon*sphereRayon;
+    var discriminant = b*b - 4.0*c;
+    //console.log(b + " "  + c);
+    if(discriminant > 0.0) {
+        var t = (-b-Math.sqrt(discriminant))/(2.0);
+        if(t > 0.0){
+            return t;
+        }
+    }
+    return 10000.0;
+}
+
+function selectedShepe(pos){
+    //console.log(pos.x + "  " + pos.y);
+    var dir = vec3.fromValues(pos.x,pos.y,1.0);
+    var ori = vec3.fromValues(0.0,0.0,2.0);
+    vec3.transformMat3(dir,dir,repMatrix);
+    vec3.multiply(dir,dir,[alpha,alpha,-1.0]);
+    vec3.normalize(dir,dir);
+    vec3.transformMat4(dir,dir,mvMatrix);
+    vec3.transformMat4(ori,ori,mvMatrix);
+
+    var tsphere = intersectSphere(dir, ori, spherePos,0.25);
+
+    console.log(tsphere);
+
+    return tsphere == 10000 ?  false :  true;
+
+}
 function handleMouseMove(event) {
-    event = event || window.event; // IE-ism
-    mousePos = {
+    var mousePos = {
         x: event.clientX,
         y: event.clientY
     };
-    if (dragging){
 
+    if (dragging){
 
         dX = mousePos.x - oldMousePos.x;
         dY = mousePos.y - oldMousePos.y;
 
-        //console.log((mousePos.x - oldMousePos.x) + ", " + (mousePos.y - oldMousePos.y)); //--- DEBUG LINE ---
+        console.log(dX + "  "+ dY);
+        rotY += dX;
+        rotX += dY;
 
-
-        rotY += dX > 0 ? rotSpeed : dX < 0 ? -rotSpeed : 0;
-        rotX += dY > 0 ? rotSpeed : dY < 0 ? -rotSpeed : 0;
-        oldMousePos = mousePos;
+        drawScene();
     }
-    drawScene();
+    if(sphereMoving){
+        dX = mousePos.x - oldMousePos.x;
+        dY = mousePos.y - oldMousePos.y;
+
+        var posY = dX > 0 ? 0.01*Math.abs(dX)*0.5 : dX < 0 ? -0.01*Math.abs(dX)*0.5 : 0;
+        var posX = dY > 0 ? -0.01*Math.abs(dY)*0.5 : dY < 0 ? 0.01*Math.abs(dY)*0.5 : 0;
+
+        //console.log(posX +" " + posY);
+        var offset = vec3.fromValues(posY,posX,0.0);
+        vec3.transformMat4(offset,offset,mvMatrix);
+        vec3.add(spherePos,spherePos,offset);
+
+        drawScene();
+    }
+    oldMousePos = mousePos;
 }
 
-function handleMouseDown(event){
-    dragging = true;
-    mouseButton = event.button;
-    oldMousePos.x = oldMousePos.y = 0;
+function getMousePos(event) {
+    var rect = canvas.getBoundingClientRect();
+    return {x: parseInt((event.clientX - rect.left)), y: parseInt((event.clientY - rect.top))};
 }
 
+function handleMouseDown(event) {
+    var pos = getMousePos(event);
+
+    sphereMoving = selectedShepe(pos);
+
+    dragging = !sphereMoving;
+    oldMousePos.x = event.clientX;
+    oldMousePos.y = event.clientY;
+}
 function handleMouseUp(event){
     dragging = false;
+    sphereMoving = false;
+
 }
